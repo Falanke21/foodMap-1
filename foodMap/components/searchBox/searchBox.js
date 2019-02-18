1d65214148b9926b3e1e1fd388d5b012e2e65cb4
Jefferson Zhong <jefferson.zhong@mail.utoronto.ca>
2019-02-18 16:43:58 -0500

    Intiailized the searchBox components.

---
+++ foodMap/components/searchBox/searchBox.js
@@ -0,0 +1,25 @@
+// components/searchBox/searchBox.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+    flag: true,
+
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+    
+  }
+})
