d9c4d43aa5957f5d6decae8929df15694318b694
palinpaalin <peilin.ye@mail.utoronto.ca>
2019-02-23 17:48:54 -0500

    databse communication

--- foodMap/pages/data/data.js
+++ foodMap/pages/data/data.js
@@ -1,104 +1,61 @@
-// pages/data/data.js
+// miniprogram/pages/testGetData/testGetData.js
 Page({
 
   /**
-   * 页面的初始数据
-   */
+  * Page initial data
+  */
   data: {
-
+    long: 0,
+    lat: 0,
+    address: 'hello',
+    test: null,
   },
 
   /**
-   * 生命周期函数--监听页面加载
-   */
+  * Lifecycle function--Called when page load
+  */
   onLoad: function (options) {
+    wx.cloud.init()
+    const db = wx.cloud.database()
+    var that = this
+    that.setData({
+      long: 200,
+      lat: 200,
+      address: 'whats up'
+    })
+    /** 
+    db.collection('location').doc('new bahen').update({
+    data:{
+    name: 'test success'
+    },
+    success(res){
+    console.log(res.data)
+    that.setData({
+    test: 250,
+    lat: 100,
+    address: 'success'
+    })
+    }
+    })
+    */
 
-  },
-
-  user_info_request: function () {
-    var that = this;
-    var user_id = wx.getStorageSync('userid');
-    wx.request({
-      //请求链接 //这里需要改
-      url: 'http://wechat.square.com/WechatUser/get_user/userid/',
-      //发送的数据 
-      data: {
-        user_id: user_id
-      },
-      //成功回调
-      success: function (res) {
+    db.collection('location').where({
+      name: "bahen"
+    }).get({
+      success(res) {
+        console.log(res)
+        console.log(res.data)
+        console.log(res.data.name)
         that.setData({
-          //具体名称要改 //在原本的page里就要有这些variable //记得加上
-          user_name: res.data.data.nickname,
-          user_level: res.data.data.level,
-          user_exp: res.data.data.exp
+          long: 100,
+          lat: 100,
+          address: 'success',
+          test: res.data[0].name
         })
-      },
-    })
-
-  },
-
-  populate_map_request: function () {
-    var that = this;
-    wx.request({
-      url: 'http://localhost:443/getMap',
-      method: 'GET',
-      //header 需要吗？
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
-      success: function (res) {
-        console.log(res.data)//打印到控制台
-        var marker_list = res.data.marker_list;
-        if (marker_list == null) {
-          var toastText = '数据获取失败';
-          wx.showToast({
-            title: toastText,
-            icon: '',
-            duration: 2000
-          });
-        } else {
-          //一个for loop function
-          create_marker(list);
-        }
       }
     })
+
   },
 
-  location_info: function () {
-    var that = this;
-    wx.request({
-      url: 'http://localhost:443/getWord',
-      data: {
-        //rest_id应该要在page的data里define，
-        // 在marker onTapde1时候set好
-        restarunt_id: that.data.restarunt_id
-      },
-      method: 'GET',
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
-      success: function (res) {
-        console.log(res.data)//打印到控制台
-        var message = res.data.message;
-        if (message == null) {
-          var toastText = '数据获取失败';
-          wx.showToast({
-            title: toastText,
-            icon: '',
-            duration: 2000
-          });
-        } else {
-          that.setData({
-            rest_name: res.data.data.restarunt_name,
-            rest_location: res.data.data.restarunt_location,
-            rest_likes: res.data.data.restarunt_likes,
-            rest_info: res.data.data.restarunt_info,
-          })
-        }
-      }
-    })
-  },
-  
 
 })
