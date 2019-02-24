7c1de0ecac1105de911e7ec1c27c1c0784f2ebc7
palinpaalin <peilin.ye@mail.utoronto.ca>
2019-02-23 19:36:59 -0500

    remove unused sql java

--- foodMap/database/moca.sql
+++
@@ -1,9 +0,0 @@
-Create table Store_Fix (ID int not null primary key, name varchar(255), discription varchar(255), latitude float, longitude float, address varchar(255))
-Create table Store_Flex (ID int not null primary key, sum_of_like int, comment varchar(255))
-Create table User (UID int not null primary key, PersonId int, City varchar(255), State varchar(255))
-
-
-Truncate table Person
--- insert into Person (PersonId, LastName, FirstName) values ('1', 'Wang', 'Allen')
-Truncate table Address
--- insert into Address (AddressId, PersonId, City, State) values ('1', '2', 'New York City', 'New York')
