315e717646677c6903adfafc923037fd63e48134
Falanke21 <1065314944@qq.com>
2019-03-02 17:31:45 -0500

    rough design for merchant page

--- foodMap/pages/popupPage/popupPage.js
+++ foodMap/pages/popupPage/popupPage.js
@@ -7,8 +7,7 @@ Page({
   data: {
     flag: true,
     likes: 0,
-    imageUrl: ["https://pbs.twimg.com/profile_images/558329813782376448/H2cb-84q_400x400.jpeg"],
-    images: ["https://pbs.twimg.com/profile_images/558329813782376448/H2cb-84q_400x400.jpeg", "https://i.ytimg.com/vi/3flWbxiQeY4/maxresdefault.jpg"]
+    images: ["https://6d6f-moca-map-3c18df-1258691048.tcb.qcloud.la/maxresdefault.jpg?sign=b4fed99b8b4ac597f6a981d80608359a&t=1551563642", "https://pbs.twimg.com/profile_images/558329813782376448/H2cb-84q_400x400.jpeg", "https://i.ytimg.com/vi/3flWbxiQeY4/maxresdefault.jpg", "https://pbs.twimg.com/media/CpHIAjtWYAERUMP.jpg"]
   },
 
   //回到地图
