edee58058094f3b27bdb73f53f2f94beb8994858
palinpaalin <peilin.ye@mail.utoronto.ca>
2019-03-24 15:20:01 -0400

    Get userInfo in popuppage

--- foodMap/pages/popupPage/popupPage.js
+++ foodMap/pages/popupPage/popupPage.js
@@ -1,10 +1,16 @@
 // pages/popupPage/popupPage.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    userInfo: {},
+    hasUserInfo: false,
+    nickName:'',
+    avatarUrl:'',
+    openId:'',
     flag: true,
     likes: 0,
     name: 'defa_name',
@@ -32,6 +38,11 @@ Page({
     this.setData({
       likes: ++this.data.likes
     })
+    this.addExp()
+  },
+
+  addExp() {
+    var that = this
   },
 
   navigate: function(e){
@@ -54,6 +65,34 @@ Page({
       mkid: dbid
     })
     console.log(that.data.mkid)
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true,
+            nickName: res.userInfo.nickName,
+            avatarUrl: res.userInfo.avatarUrl
+          })
+        }
+      })
+    }
   },
 
   /**
