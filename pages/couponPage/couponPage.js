3423ea86ff2f5f0feb01b2d9c66a5d7293acdce6
dino <dino.li@mail.utoronto.ca>
2019-06-01 15:58:34 -0400

    Working on

--- pages/couponPage/couponPage.js
+++ pages/couponPage/couponPage.js
@@ -1,66 +1,58 @@
-// pages/couponPage/couponPage.js
+
+const MONTHS = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May.', 'June.', 'July.', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.'];
 Page({
 
   /**
-   * Page initial data
+   * 页面的初始数据
    */
   data: {
+    year: new Date().getFullYear(),      // 年份
+    month: new Date().getMonth() + 1,    // 月份
+    day: new Date().getDate(),
+    str: MONTHS[new Date().getMonth()],  // 月份字符串
 
+    merchandiseList: ["no data in cloud"],
+    url_lis: []
   },
 
   /**
-   * Lifecycle function--Called when page load
+   * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
   },
 
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
   onReady: function () {
+    this.popup = this.selectComponent("#product");
+    const db = wx.cloud.database();
 
+    var that = this;
+
+    db.collection('location').get({
+      success(res) {
+        // res.data 是一个包含集合中有权限访问的所有记录的数据，不超过 20 条
+        that.setData({
+          merchandiseList: res.data,
+        });
+      }
+    })
+
+    db.collection('img_url').where({
+      type: "location_img_url"
+    }).get({
+      success(res) {
+        that.setData({
+          url_lis: res.data[0],
+        })
+      }
+    })
+
+    console.log(this.data.url_lis)
   },
 
-  /**
-   * Lifecycle function--Called when page show
-   */
-  onShow: function () {
-
+  showProduct: function (e) {
+    var dbid = e.currentTarget.dataset.index + 1;
+    wx.navigateTo({
+      url: '../productPage/product?id=' + dbid,
+    })
   },
-
-  /**
-   * Lifecycle function--Called when page hide
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page unload
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * Page event handler function--Called when user drop down
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * Called when page reach bottom
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * Called when user click on the top right corner to share
-   */
-  onShareAppMessage: function () {
-
-  }
 })
