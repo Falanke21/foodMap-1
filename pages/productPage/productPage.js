58dd368c516d871579bb700acbac89546d3a0361
Falanke21 <1065314944@qq.com>
2019-07-06 15:32:59 -0400

    add all purchase coupon logic

--- pages/productPage/productPage.js
+++ pages/productPage/productPage.js
@@ -11,7 +11,10 @@ Page({
     id: 0,
     minusStatus:true,
     addedNum:1,
-    couponObject: null
+    couponObject: null,
+    userCredit: -1,
+    creditNeed: 0,
+    couponsToAppend: [],
   },
 
   /**
@@ -32,6 +35,7 @@ Page({
         console.log(res.data[0])
         that.setData({
           couponObject: res.data[0],
+          creditNeed: res.data[0].credit,
         })
       },
     })
@@ -42,6 +46,27 @@ Page({
    */
   onReady: function () {
     console.log("This product is of id: " + this.data.id)
+
+    var that = this;
+    db.collection('wxuser').where({
+      _openid: app.globalData.openid
+    }).get({
+      success(res) {
+        console.log(res)
+        that.setData({
+          userCredit: res.data[0].credit
+        })
+        console.log("userCredit is " + that.data.userCredit)
+      },
+      fail: err => {
+        icon: 'none',
+          console.error('获取credit失败', err)
+        wx.showToast({
+          title: '获取credit失败',
+          icon: "none",
+        })
+      }
+    })
   },
 
   /**
@@ -65,9 +90,28 @@ Page({
 
   },
 
-  exchange:function(){
+  exchange: function(){
     var that = this;
 
+    wx.showLoading({
+      title: 'Loading',
+    })
+
+    if (this.data.creditNeed > this.data.userCredit) {
+      wx.showToast({
+        title: '您的Credit不足',
+        icon: 'none',
+      })
+      return
+    }
+
+    var temp = [];
+    for (var i = 0; i < that.data.addedNum; i++) {
+      temp.push(that.data.id)
+    }
+    this.setData({
+      couponsToAppend: temp,
+    })
     const _ = db.command
 
     db.collection('wxuser').where({
@@ -76,12 +120,16 @@ Page({
       success(res) {
         db.collection('wxuser').doc(res.data[0]._id).update({
           data: {
-            wallet: _.push(that.data.id)
+            wallet: _.push(that.data.couponsToAppend),
+            credit: _.inc(-1 * that.data.creditNeed)
           },
           success: function (res) {
             wx.showToast({
               title: '成功购买！',
               icon: "success",
+              complete: wx.navigateBack({
+                delta: 1,
+              })
             })
           },
           fail: err => {
@@ -108,7 +156,8 @@ Page({
     courseCount++;
     this.setData({
       addedNum: courseCount,
-      minusStatus: false
+      minusStatus: false,
+      creditNeed: courseCount * this.data.couponObject.credit,
     })
   },
 
@@ -121,7 +170,8 @@ Page({
     var minusStatus = courseCount <= 1 ? true : false;
     this.setData({
       addedNum: courseCount,
-      minusStatus: minusStatus
+      minusStatus: minusStatus,
+      creditNeed: courseCount * this.data.couponObject.credit,
     });
   }
 
